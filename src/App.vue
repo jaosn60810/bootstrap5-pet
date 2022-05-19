e2cb63f89fd7c052d3f3fb88b2c24d07f585ad30
jason60810 <jason60810@gmail.com>
2022-05-19 23:56:46 +0800

    feat: 完成 Footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <Navbar></Navbar>
   <router-view></router-view>
   <ContactSection></ContactSection>
+  <Footer></Footer>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
 import ContactSection from '@/components/ContactSection.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   components: {
     Navbar,
     ContactSection,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="py-4 bg-dark">
+    <p class="text-center text-white mb-0">
+      Copyright © 2021 撿到寶.All rights reserved.
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
