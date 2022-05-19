9200d19d48ecf3c400fb730a68e463aa0b3dd462
jason60810 <jason60810@gmail.com>
2022-05-19 17:55:30 +0800

    fix: 修正路徑

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <Navbar></Navbar>
   <router-view></router-view>
+  <ContactSection></ContactSection>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import ContactSection from '@/components/ContactSection.vue';
+
 export default {
   components: {
     Navbar,
+    ContactSection,
   },
 };
 </script>
