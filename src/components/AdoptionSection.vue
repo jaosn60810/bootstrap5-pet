7eb881572498a8bb2c02573c6461343a3672d531
jason60810 <jason60810@gmail.com>
2022-05-19 17:18:31 +0800

    feat: 完成 AdoptionSection

---
+++ src/components/AdoptionSection.vue
@@ -0,0 +1,119 @@
+<template>
+  <section class="py-6 py-lg-11">
+    <div class="d-flex justify-content-center mb-3 mb-lg-4">
+      <img src="@/assets/img/title02.png" alt="領養專區" />
+    </div>
+
+    <h2 class="text-center h6 mb-5 mb-lg-7">
+      這裡是在還在等家的浪浪們，若您正巧在找個家人，不訪來認識一下唄
+    </h2>
+
+    <div class="container mb-5 mb-lg-7">
+      <div class="row row-cols-1 row-cols-lg-3 justify-content-center gy-3">
+        <div class="col">
+          <div class="card rounded-10 overflow-hidden">
+            <div class="position-relative mb-5">
+              <img
+                src="../assets/img/dog01.png"
+                class="opacity-100 hoverImgHide img-fluid w-100"
+                alt="dog01"
+              />
+              <img
+                src="../assets/img/dog01-hover-1.png"
+                class="opacity-0 position-absolute top-0 start-0 end-0 hoverImgShow img-fluid w-100"
+                alt="dog01-hover"
+              />
+            </div>
+
+            <div class="card-body px-4 py-0">
+              <h3 class="card-title fw-bold h6 mb-3">等家中-華仔</h3>
+              <p class="card-text border-bottom pb-4">
+                目前7個月大的華仔，是個愛笑的貪吃鬼，喜歡吃西瓜還有蓮霧，是一隻吃素的乖狗...
+              </p>
+            </div>
+
+            <a
+              href="#"
+              class="text-dark text-decoration-none hoverTextPrimary fontKalam d-block py-3 text-center"
+              >More</a
+            >
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card rounded-10 overflow-hidden">
+            <div class="position-relative mb-5">
+              <img
+                src="../assets/img/04.png"
+                class="opacity-100 hoverImgHide img-fluid w-100"
+                alt="dog04"
+              />
+              <img
+                src="../assets/img/dog01-hover.png"
+                class="opacity-0 position-absolute top-0 start-0 end-0 hoverImgShow img-fluid w-100"
+                alt="dog01-hover"
+              />
+            </div>
+
+            <div class="card-body px-4 py-0">
+              <h3 class="card-title fw-bold h6 mb-3">等家中-小呆</h3>
+              <p class="card-text border-bottom pb-4">
+                小呆有著圓圓的大眼睛，一歲的他來自台中，原本陪著一位老奶奶生活，上個月...
+              </p>
+            </div>
+
+            <a
+              href="#"
+              class="text-dark text-decoration-none hoverTextPrimary fontKalam d-block py-3 text-center"
+              >More</a
+            >
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card rounded-10 overflow-hidden">
+            <div class="position-relative mb-5">
+              <img
+                src="../assets/img/08.png"
+                class="opacity-100 hoverImgHide img-fluid w-100"
+                alt="dog08"
+              />
+              <img
+                src="../assets/img/dog01-hover-2.png"
+                class="opacity-0 position-absolute top-0 start-0 end-0 hoverImgShow img-fluid w-100"
+                alt="dog01-hover"
+              />
+            </div>
+
+            <div class="card-body px-4 py-0">
+              <h3 class="card-title fw-bold h6 mb-3">等家中-Snow</h3>
+              <p class="card-text border-bottom pb-4">
+                看起來很兇但特別怕打雷的Snow，很喜歡吃冰，冰冰的水也喜歡，如果摸他的額頭...
+              </p>
+            </div>
+
+            <a
+              href="#"
+              class="text-dark text-decoration-none hoverTextPrimary fontKalam d-block py-3 text-center"
+              >More</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-center">
+      <router-link
+        class="btn btn-outline-primary text-dark border-dark hoverBorderPrimary py-2 px-5 rounded-pill"
+        to="/services"
+        >看看更多浪浪</router-link
+      >
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
   <main class="backgroundImage">
     <ServiceSection></ServiceSection>
     <RecentWork></RecentWork>
+    <AdoptionSection></AdoptionSection>
   </main>
 </template>
 
@@ -11,12 +12,14 @@
 import Header from '@/components/Header.vue';
 import ServiceSection from '@/components/ServiceSection.vue';
 import RecentWork from '@/components/RecentWork.vue';
+import AdoptionSection from '@/components/AdoptionSection.vue';
 
 export default {
   components: {
     Header,
     ServiceSection,
     RecentWork,
+    AdoptionSection,
   },
 };
 </script>
