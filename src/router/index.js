5d30263985aeccdfe6a399cb1b5a539a7eba4f4d
jason60810 <jason60810@gmail.com>
2022-05-19 18:40:01 +0800

    feat: 新增動態 title

--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,39 @@ const routes = [
   {
     path: '/',
     component: () => import('../views/Home.vue'),
+    meta: { title: '撿到寶 | 首頁' },
   },
   {
     path: '/services',
     component: () => import('../views/Services.vue'),
+    meta: { title: '撿到寶 | 服務介紹' },
   },
   {
     path: '/design-works',
     component: () => import('../views/DesignWorks.vue'),
+    meta: { title: '撿到寶 | 設計作品' },
   },
   {
     path: '/adoption-area',
     component: () => import('../views/AdoptionArea.vue'),
+    meta: { title: '撿到寶 | 領養專區' },
   },
   {
     path: '/order',
     component: () => import('../views/Order.vue'),
+    meta: { title: '撿到寶 | 毛孩似繪顏' },
   },
 ];
-export default createRouter({
+
+const router = createRouter({
   history: createWebHashHistory(),
   routes,
   linkActiveClass: 'active',
 });
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+
+export default router;
