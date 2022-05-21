6ef2e43f8641c61b110f15da8296096833338e90
jason60810 <jason60810@gmail.com>
2022-05-21 23:57:33 +0800

    feat: 完成 Services

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,7 +1,91 @@
 <template>
-  <div>
-    <h1>Services</h1>
-  </div>
+  <section class="backgroundImage">
+    <div class="w-lg-100 mb-6 mb-lg-11 overflow-hidden">
+      <img
+        src="@/assets/img/banner.png"
+        alt="毛孩似繪言"
+        class="w-100 d-none d-lg-block"
+      />
+      <img
+        src="@/assets/img/banner_service-sm.png"
+        alt="毛孩似繪言"
+        class="w-100 d-lg-none"
+      />
+    </div>
+    <h1 class="d-none">毛孩似繪言</h1>
+
+    <div class="container mb-7 mb-lg-11">
+      <ul class="row row-cols-1 row-cols-lg-4 gy-8 gy-lg-0 list-unstyled">
+        <li class="col">
+          <div class="d-flex flex-row flex-lg-column align-items-center">
+            <img
+              src="../assets/img/step01.png"
+              class="w-30 w-lg-75 me-3 me-lg-0 mb-lg-3"
+              alt="..."
+            />
+
+            <div class="text-lg-center">
+              <h4 class="text-primary h5 fontKalam mb-0">STEP1</h4>
+              <h2 class="h5 fw-bold mb-1 mb-lg-3">購買似顏繪服務</h2>
+              <h3 class="h6 mb-0">填寫購買表單</h3>
+              <h3 class="h6 mb-0">並完成轉帳付款</h3>
+            </div>
+          </div>
+        </li>
+
+        <li class="col">
+          <div class="d-flex flex-row flex-lg-column align-items-center">
+            <img
+              src="../assets/img/step02.png"
+              class="w-30 w-lg-75 me-3 me-lg-0 mb-lg-3"
+              alt="..."
+            />
+
+            <div class="text-lg-center">
+              <h4 class="text-primary h5 fontKalam mb-0">STEP2</h4>
+              <h2 class="h5 fw-bold mb-1 mb-lg-3">設計師繪製</h2>
+              <h3 class="h6 mb-0">設計師繪製似顏繪</h3>
+              <h3 class="h6 mb-0">不提供改稿</h3>
+            </div>
+          </div>
+        </li>
+
+        <li class="col">
+          <div class="d-flex flex-row flex-lg-column align-items-center">
+            <img
+              src="../assets/img/step04.png"
+              class="w-30 w-lg-75 me-3 me-lg-0 mb-lg-3"
+              alt="..."
+            />
+
+            <div class="text-lg-center">
+              <h4 class="text-primary h5 fontKalam mb-0">STEP3</h4>
+              <h2 class="h5 fw-bold mb-1 mb-lg-3">畫作提供</h2>
+              <h3 class="h6 mb-0">電子檔案or實體化做寄送</h3>
+              <h3 class="h6 mb-0">設計師提供作品</h3>
+            </div>
+          </div>
+        </li>
+
+        <li class="col">
+          <div class="d-flex flex-row flex-lg-column align-items-center">
+            <img
+              src="../assets/img/step03.png"
+              class="w-30 w-lg-75 me-3 me-lg-0 mb-lg-3"
+              alt="..."
+            />
+
+            <div class="text-lg-center">
+              <h4 class="text-primary h5 fontKalam mb-0">STEP4</h4>
+              <h2 class="h5 fw-bold mb-1 mb-lg-3">幫助浪浪</h2>
+              <h3 class="h6 mb-0">每個月30號我們會將設計費</h3>
+              <h3 class="h6 mb-0">購買飼料，提供給xxx中途之家</h3>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
